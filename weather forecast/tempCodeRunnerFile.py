9885ee14c1f89bf438c9929120d8154b314bc63e
ADARSH KUMAR MISHRA <131741318+Adarsh-Kumar-Mishra47@users.noreply.github.com>
2024-03-18 08:34:08 +0530

    Add files via upload

---
+++ weather forecast/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+dob_lbl = tk.Label(detail_frame,text="D.O.B",font=("Arial",15),bg="lightgrey")
+dob_lbl.grid(row=8,column=0,padx=2,pady=2)
+
+dob_ent = tk.Entry(detail_frame,bd=7,font=("arial",15))
+dob_ent.grid(row=8,column=1,padx=2,pady=2)
